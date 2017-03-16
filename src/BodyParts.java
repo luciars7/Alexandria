3a75b21a1d47980b2c1588ad6525204caffc1acc
Patricia Delgado Gorostiza <patricia.delgadogorostiza@gmail.com>
2017-03-16 16:04:01 +0100

    bodyParts and diseases uploaded still not finished

---
+++ src/BodyParts.java
@@ -0,0 +1,28 @@
+public class BodyParts {
+int ID;
+String name;
+String location;
+public int getID() {
+	return ID;
+}
+public void setID(int ID) {
+	ID = ID;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+public BodyParts(int ID, String name, String location){
+	this.ID = ID;
+	this.name = name;
+	this.location = location;
+}
+}
