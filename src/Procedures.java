d368db2e953664acf2ef9855adc05cf9991814a3
cakessler <ckesslerm@gmail.com>
2017-03-16 15:40:08 +0100

    Procedures class

--- src/Procedures.java
+++ src/Procedures.java
@@ -1,4 +1,33 @@
 
 public class Procedures {
-
+	int ID;
+	String name;
+	String description;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Procedures(int iD, String name, String description) {
+		super();
+		ID = iD;
+		this.name = name;
+		this.description = description;
+	}
+	
+	
 }
