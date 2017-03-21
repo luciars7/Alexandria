03c3d1bada7797fbfc9688c6345d72598a562410
nachomcapella <nachetemc97@gmail.com>
2017-03-21 15:15:53 +0100

    Le petit commité 3

--- src/User_Interface/Command_line_user_interface.java
+++ src/User_Interface/Command_line_user_interface.java
@@ -2,14 +2,14 @@ package User_Interface;
 
 public class Command_line_user_interface {
 	public static void main(String args[]) {
-		//Create a connection object.
-		//Create DB_manager object.
-		//Show "patients".
+		// Create a connection object.
+		// Create DB_manager object.
+		// Show "patients".
 	}
-	
-	public void addPatient(){
-		//Builds a patient object.
-		//I think we have to do an add method for each POJO.
+
+	public void addPatient() {
+		// Builds a patient object.
+		// I think we have to do an add method for each POJO.
 	}
-	
+
 }
--- src/jdbc/DB_manager.java
+++ src/jdbc/DB_manager.java
@@ -6,69 +6,74 @@ import java.sql.Statement;
 
 public class DB_manager {
 	Connection c = null;
-	
-	public void connect(){
-	try {
-		// Open database connection
-		Class.forName("org.sqlite.JDBC"); 
-	    c = DriverManager.getConnection("jdbc:sqlite:./db/company.db"); //Indicates the technology and location of the database.
-		c.createStatement().execute("PRAGMA foreign_keys=ON"); //Enables the support for foreign key constraints.
-		System.out.println("Database connection opened.");
-		
-		// Here is where I do things with the database
-		
-		// Close database connection
-		c.close();
-		System.out.println("Database connection closed.");
-	} catch (Exception e) {
-		e.printStackTrace();
-	}
-  }
-    public void createTables(){
-    	try {
+
+	public void connect() {
+		try {
 			// Open database connection
-			Class.forName("org.sqlite.JDBC"); 
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:./db/company.db"); // Indicates
+																			// the
+																			// technology
+																			// and
+																			// location
+																			// of
+																			// the
+																			// database.
+			c.createStatement().execute("PRAGMA foreign_keys=ON"); // Enables
+																	// the
+																	// support
+																	// for
+																	// foreign
+																	// key
+																	// constraints.
+			System.out.println("Database connection opened.");
+
+			// Here is where I do things with the database
+
+			// Close database connection
+			c.close();
+			System.out.println("Database connection closed.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void createTables() {
+		try {
+			// Open database connection
+			Class.forName("org.sqlite.JDBC");
 			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/company.db");
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
-			
+
 			// Create tables: begin
 			Statement stmt1 = c.createStatement();
-			String sql1 = "CREATE TABLE departments "
-					   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					   + " name     TEXT     NOT NULL, "
-					   + " address  TEXT	 NOT NULL)";
+			String sql1 = "CREATE TABLE departments " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " name     TEXT     NOT NULL, " + " address  TEXT	 NOT NULL)";
 			stmt1.executeUpdate(sql1);
 			stmt1.close();
 			Statement stmt2 = c.createStatement();
-			String sql2 = "CREATE TABLE employees "
-					   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					   + " name     TEXT     NOT NULL, "
-					   + " dob      DATE	 NOT NULL, "
-					   + " address  TEXT, "
-					   + " salary   REAL,"
-					   + " photo	BLOB,"
-					   + " dep_id	INTEGER REFERENCES departments(id) ON UPDATE CASCADE ON DELETE SET NULL)";
+			String sql2 = "CREATE TABLE employees " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " name     TEXT     NOT NULL, " + " dob      DATE	 NOT NULL, " + " address  TEXT, "
+					+ " salary   REAL," + " photo	BLOB,"
+					+ " dep_id	INTEGER REFERENCES departments(id) ON UPDATE CASCADE ON DELETE SET NULL)";
 			stmt2.executeUpdate(sql2);
 			stmt2.close();
 			Statement stmt3 = c.createStatement();
-			String sql3 = "CREATE TABLE reports "
-					   + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					   + " name     TEXT     NOT NULL, "
-					   + " content  TEXT  	NOT NULL, "
-					   + " date		DATE)";
+			String sql3 = "CREATE TABLE reports " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
+					+ " name     TEXT     NOT NULL, " + " content  TEXT  	NOT NULL, " + " date		DATE)";
 			stmt3.executeUpdate(sql3);
 			stmt3.close();
 			Statement stmt4 = c.createStatement();
 			String sql4 = "CREATE TABLE authors "
-					   + "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					   + " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					   + " PRIMARY KEY (report_id,employee_id))";
+					+ "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+					+ " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
+					+ " PRIMARY KEY (report_id,employee_id))";
 			stmt4.executeUpdate(sql4);
 			stmt4.close();
 			System.out.println("Tables created.");
 			// Create table: end
-			
+
 			// - Set initial values for the Primary Keys
 			// - Don't try to understand this until JPA is explained
 			// This is usually not needed, since the initial values
@@ -83,13 +88,12 @@ public class DB_manager {
 			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('reports', 1)";
 			stmtSeq.executeUpdate(sqlSeq);
 			stmtSeq.close();
-			
+
 			// Close database connection
 			c.close();
 			System.out.println("Database connection closed.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    }
+	}
 }
-
