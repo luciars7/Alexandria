1b8f62774bec5abc766edf77c21e29a0f73399a0
nachomcapella <nachetemc97@gmail.com>
2017-03-23 15:32:43 +0100

    DBManager

--- src/jdbc/DBManager.java
+++ src/jdbc/DBManager.java
@@ -2,41 +2,39 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBManager {
 	Connection c = null;
 
+    public void DBManagar(){
+    	connect();
+    }
+	
 	public void connect() {
 		try {
 			// Open database connection
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:./db/company.db"); // Indicates
-																			// the
-																			// technology
-																			// and
-																			// location
-																			// of
-																			// the
-																			// database.
-			c.createStatement().execute("PRAGMA foreign_keys=ON"); // Enables
-																	// the
-																	// support
-																	// for
-																	// foreign
-																	// key
-																	// constraints.
+			c = DriverManager.getConnection("jdbc:sqlite:./db/company.db"); 
+			c.createStatement().execute("PRAGMA foreign_keys=ON"); 
 			System.out.println("Database connection opened.");
-
-			// Here is where I do things with the database
-
-			// Close database connection
-			c.close();
-			System.out.println("Database connection closed.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	public void close(){
+		// Close database connection
+					try {
+						c.close();
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println("Database connection closed.");
+	}
+	
 
 	public void createTables() {
 		try {
