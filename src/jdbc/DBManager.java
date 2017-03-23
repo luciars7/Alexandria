7e6727ea10867f54a35344f23d7181cd17942998
Patricia Delgado Gorostiza <patricia.delgadogorostiza@gmail.com>
2017-03-23 16:29:57 +0100

    insert authors

--- src/jdbc/DBManager.java
+++ src/jdbc/DBManager.java
@@ -3,6 +3,7 @@ package jdbc;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBManager {
