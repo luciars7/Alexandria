cf03befcd5087f926b6e2392dbf8fcef19f6fb6e
Patricia Delgado Gorostiza <patricia.delgadogorostiza@gmail.com>
2017-03-23 15:18:13 +0100

    Db manager

--- src/jdbc/DBManager.java
+++ src/jdbc/DBManager.java
@@ -41,42 +41,130 @@ public class DBManager {
 	public void createTables() {
 		try {
 			// Open database connection
-			/*Class.forName("org.sqlite.JDBC");
+			Class.forName("org.sqlite.JDBC");
 			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/company.db");
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
 
 			// Create tables: begin
+           
+		
 			Statement stmt1 = c.createStatement();
-			String sql1 = "CREATE TABLE departments " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					+ " name     TEXT     NOT NULL, " + " address  TEXT	 NOT NULL)";
+			String sql1 = "CREATE TABLE papers "
+					+ "(ID INTEGER PRIMARY KEY,"
+					+ " title  TEXT,"
+					+ "source TEXT)";
 			stmt1.executeUpdate(sql1);
 			stmt1.close();
+			System.out.println("Tables created.");
+			
 			Statement stmt2 = c.createStatement();
-			String sql2 = "CREATE TABLE employees " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					+ " name     TEXT     NOT NULL, " + " dob      DATE	 NOT NULL, " + " address  TEXT, "
-					+ " salary   REAL," + " photo	BLOB,"
-					+ " dep_id	INTEGER REFERENCES departments(id) ON UPDATE CASCADE ON DELETE SET NULL)";
+			String sql2 = "CREATE TABLE body parts "
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " name  TEXT,"
+					+ "location TEXT)";
 			stmt2.executeUpdate(sql2);
 			stmt2.close();
+			
 			Statement stmt3 = c.createStatement();
-			String sql3 = "CREATE TABLE reports " + "(id       INTEGER  PRIMARY KEY AUTOINCREMENT,"
-					+ " name     TEXT     NOT NULL, " + " content  TEXT  	NOT NULL, " + " date		DATE)";
+			String sql3 = "CREATE TABLE diseases "
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " name  TEXT,"
+					+ "description TEXT,"
+					+ "body parts TEXT REFERENCES body parts (ID) ON UPDATE CASCADE ON DELETE CASCADE)";
 			stmt3.executeUpdate(sql3);
 			stmt3.close();
+			
 			Statement stmt4 = c.createStatement();
 			String sql4 = "CREATE TABLE authors "
-					+ "(report_id     INTEGER  REFERENCES reports(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					+ " employee_id   INTEGER  REFERENCES employees(id) ON UPDATE CASCADE ON DELETE SET NULL,"
-					+ " PRIMARY KEY (report_id,employee_id))";
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " name  TEXT,"
+					+ "origin TEXT,"
+					+ "association TEXT)";
 			stmt4.executeUpdate(sql4);
 			stmt4.close();
-			*/
+			
 			Statement stmt5 = c.createStatement();
-			String sql5 = "CREATE TABLE body parts" +"( )";
-			System.out.println("Tables created.");
+			String sql5 = "CREATE TABLE devices "
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " name  TEXT,"
+					+ "type TEXT ,"
+					+ "price($) FLOAT,"
+					+ "brand TEXT,"
+					+ "med procedures INTEGER REFERENCES procedures (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "papers INTEGER REFERENCES papers (ID) ON UPDATE CASCADE ON DELETE CASCADE)";
+			stmt5.executeUpdate(sql5);
+			stmt5.close();
+			
+			Statement stmt6 = c.createStatement();
+			String sql6 = "CREATE TABLE images "
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " description,"
+					+ "type TEXT,"
+					+ "size TEXT,"
+					+ "image BLOOB,"
+					+ "paper INTEGER REFERENCES papers (ID) ON UPDATE CASCADE ON DELETE CASCADE)";
+			stmt6.executeUpdate(sql6);
+			stmt6.close();
+			
+			Statement stmt7 = c.createStatement();
+			String sql7 = "CREATE TABLE symptoms "
+					+ "(ID     INTEGER PRIMARY KEY,"
+					+ " name TEXT,"
+					+ "description TEXT)";
+			stmt7.executeUpdate(sql7);
+			stmt7.close();
+			
+			Statement stmt8 = c.createStatement();
+			String sql8 = "CREATE TABLE procedures"
+					+ "(ID INTEGER PRIMARY KEY,"
+					+ " name TEXT,"
+					+ "description TEXT)";
+			stmt8.executeUpdate(sql8);
+			stmt8.close();
+			
+			Statement stmt9 = c.createStatement();
+			String sql9 = "CREATE TABLE symptoms-diseases"
+					+ "(diseases INTEGER REFERENCES diseases (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ " symptoms INTEGER REFERENCES symptoms (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "PRIMARY KEY (diseases,symptoms))";
+			stmt9.executeUpdate(sql9);
+			stmt9.close();
+			
+			Statement stmt10 = c.createStatement();
+			String sql10 = "CREATE TABLE papers-authors"
+					+ "(papers INTEGER REFERENCES papers (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ " authors INTEGER REFERENCES authors (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "PRIMARY KEY (papers,authors))";
+			stmt10.executeUpdate(sql10);
+			stmt10.close();
+			
+			Statement stmt11 = c.createStatement();
+			String sql11 = "CREATE TABLE papers-diseases"
+					+ "(papers INTEGER REFERENCES papers (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ " diseases INTEGER REFERENCES diseases (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "PRIMARY KEY (papers,diseases))";
+			stmt11.executeUpdate(sql11);
+			stmt11.close();
+			
+			Statement stmt12 = c.createStatement();
+			String sql12 = "CREATE TABLE images-diseases"
+					+ "(images INTEGER REFERENCES images (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ " diseases INTEGER REFERENCES diseases (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "PRIMARY KEY (images,diseases))";
+			stmt12.executeUpdate(sql12);
+			stmt12.close();
+			
+			Statement stmt13 = c.createStatement();
+			String sql13 = "CREATE TABLE procedures-diseases"
+					+ "(procedures INTEGER REFERENCES procedures (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ " diseases INTEGER REFERENCES diseases (ID) ON UPDATE CASCADE ON DELETE CASCADE,"
+					+ "PRIMARY KEY (procedures,diseases))";
+			stmt13.executeUpdate(sql13);
+			stmt13.close();
 			// Create table: end
 
+			
 			// - Set initial values for the Primary Keys
 			// - Don't try to understand this until JPA is explained
 			// This is usually not needed, since the initial values
@@ -84,20 +172,18 @@ public class DBManager {
 			// are using JPA and JDBC in the same project, and JPA
 			// needs an initial value, we do this.
 			Statement stmtSeq = c.createStatement();
-			String sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('departments', 1)";
+			String sqlSeq = "INSERT INTO authors (ID,name,origin,association) VALUES (1, 'Christian Nordqvist', 'UK' ,'Medical News Today')";
 			stmtSeq.executeUpdate(sqlSeq);
-			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('employees', 1)";
+			sqlSeq = "INSERT INTO authors (ID,name,origin,association) VALUES (2, 'Pradeep Arora', 'USA' ,'Arora Psychiatric Consultation')";
 			stmtSeq.executeUpdate(sqlSeq);
 			sqlSeq = "INSERT INTO sqlite_sequence (name, seq) VALUES ('reports', 1)";
 			stmtSeq.executeUpdate(sqlSeq);
 			stmtSeq.close();
-
-			// Close database connection
-			c.close();
+			
+			
 			System.out.println("Database connection closed.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 }
