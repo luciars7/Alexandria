939b2087d59bac3b34b8d499a3f6a34b74fac00b
luciars7 <luciaruizs@gmail.com>
2017-05-29 11:42:50 +0200

    Symptom checked. Procedure checked. Paper checked. Image checked. Disease checked. BodyPart checked. Device checked. Author checked. POJOS DONE

--- src/pojos/Author.java
+++ src/pojos/Author.java
@@ -37,162 +37,102 @@ public class Author implements Serializable {
 	@XmlElement(name = "paper")
 	@XmlElementWrapper(name = "papers")
 	private List<Paper> paper;
+	
 	public Author() {
-
 		this.paper = new ArrayList<Paper>();
-
 	}
 
 	public Author(int iD, String name, String origin, String association) {
-
 		ID = iD;
-
 		this.name = name;
-
 		this.origin = origin;
-
 		this.association = association;
-
 		this.paper = new ArrayList<Paper>();
-
 	}
 
 	public Author(String name2, String origin2, String association2) {
-
 		this.name = name2;
-
 		this.origin = origin2;
-
 		this.association = association2;
-
 		this.paper = new ArrayList<Paper>();
-
 	}
 
 	public static long getSerialversionuid() {
-
 		return serialVersionUID;
-
 	}
 
 	public void addPaper(Paper paper) {
-
 		this.paper.add(paper);
-
 	}
 
 	public void removePaper(Paper paper) {
-
 		this.paper.remove(paper);
-
 	}
 
 	public List<Paper> getPaper() {
-
 		return paper;
-
 	}
 
 	public void setPaper(List<Paper> paper) {
-
 		this.paper = paper;
-
 	}
 
 	public int getID() {
-
 		return ID;
-
 	}
 
 	public void setID(int iD) {
-
 		ID = iD;
-
 	}
 
 	public String getName() {
-
 		return name;
-
 	}
 
 	public void setName(String name) {
-
 		this.name = name;
-
 	}
 
 	public String getOrigin() {
-
 		return origin;
-
 	}
 
 	public void setOrigin(String origin) {
-
 		this.origin = origin;
-
 	}
 
 	public String getAssociation() {
-
 		return association;
-
 	}
 
 	public void setAssociation(String association) {
-
 		this.association = association;
-
 	}
 
 	@Override
-
 	public int hashCode() {
-
 		final int prime = 31;
-
 		int result = 1;
-
 		result = prime * result + ID;
-
 		return result;
-
 	}
 
 	@Override
-
 	public boolean equals(Object obj) {
-
 		if (this == obj)
-
 			return true;
-
 		if (obj == null)
-
 			return false;
-
 		if (getClass() != obj.getClass())
-
 			return false;
-
 		Author other = (Author) obj;
-
 		if (ID != other.ID)
-
 			return false;
-
 		return true;
-
 	}
 
 	@Override
-
 	public String toString() {
-
-		return "[ID=" + ID + ", name=" + name + ", origin=" + origin + ", association=" + association + "]";
-
+		return "[ID = " + ID + ", name = " + name + ", origin = " + origin + ", association = " + association + "]";
 	}
-
 }
